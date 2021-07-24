71083c6df34d03299fe1835d6fa8b3a7cbf09874
swastika2013 <87856558+swastika2013@users.noreply.github.com>
2021-07-24 21:13:10 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,37 @@
+
+function setup() {
+  createCanvas(400,400);
+}
+
+function draw() 
+{
+  background(255);
+if(keyIsDown(UP_ARROW))
+{background("blue")
+}
+
+if(keyIsDown(LEFT_ARROW))
+{
+
+  background("green")
+}
+
+if(keyIsDown(RIGHT_ARROW))
+{
+
+  background("purple")
+}
+
+
+if(keyIsDown(DOWN_ARROW))
+{
+
+  background("red")
+}
+
+}
+
+
+
+
+ 
